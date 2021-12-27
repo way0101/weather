83d6f61ad1335da674f21741049e096d504137c4
jihyeon kil <jihyeon2085@gmail.com>
2021-12-27 17:41:28 +0900

    error handling

---
+++ script.js
@@ -0,0 +1,96 @@
+let news = []
+let menuBtn = document.querySelectorAll(".menu-btn")
+menuBtn.forEach((menu)=> menu.addEventListener("click", (e) =>getNewsByTopic(e)))
+
+let searchBtn = document.querySelector(".search-btn")
+let url;
+console.log(searchBtn)
+
+
+// 각 함수에서 필요한 url을 만든다
+// api 호출 함수를 부른다
+const getNews = async() => {
+    try{
+        let header = new Headers({'x-api-key':'HczDzcDhY32VygHq3U67QzUBcCIS8qz4xEzIDLj5smY'})
+        let response = await fetch(url,{headers:header});
+        let data = await response.json()
+        if(response.status === 200){
+            if(data.total_hits === 0){
+                throw new Error("검색된 결과 값이 없습니다.")
+            }
+            news = data.articles
+            console.log(news)
+            render()
+
+        }else{
+            throw new Error(data.message)
+        }
+    }catch(error){
+        console.log("잡힌 에러는", error.message)
+        errorRender(error.message)
+    }
+    
+}
+
+const getLatestNews = async() =>{
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=sport&page_size=4`)
+    getNews()
+}
+
+const getNewsByKeyword = async () => {
+    //1. 검색 키워드 읽어오기
+    //2. url에 검색 키워드 붙이기
+    //3. 헤더준비
+    //4. url부르기
+    //5. 데이터 가져오기
+    //6. 데이터 보여주기
+
+
+    let keyword = document.querySelector("#search").value
+    console.log(keyword)
+    url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=4`)
+    getNews()
+}
+
+//토픽
+const getNewsByTopic = async (e) =>{
+    console.log("클릭됨", e.target.textContent)
+    let topic = e.target.textContent.toLowerCase()
+    url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${topic}&page_size=4`)
+    getNews()
+}
+
+
+
+//render 함수
+
+const render = () => {
+    let newsHTML = ''
+    newsHTML = news.map((item) =>{
+        return `<div class="row news">
+        <div class="col-lg-4">
+            <img class="image-size" src="${item.media}" alt="">
+        </div>
+        <div class="col-lg-8">
+            <h2>${item.title}</h2>
+            <p>${item.summary}</p>
+            <div>
+                ${item.rights} * ${item.published_date}
+            </div>
+        </div>
+    </div>`
+    }).join('');
+
+    document.querySelector('#news-board').innerHTML = newsHTML
+}
+
+const errorRender = (message) =>{
+    let errorHTML = `<div>${message}</div>`
+    document.querySelector('#news-board').innerHTML = errorHTML
+}
+
+
+
+searchBtn.addEventListener("click", getNewsByKeyword)
+getLatestNews();
+
